cfde74b3a4123ed13b30deb667da01094210fc79
Darey Blitz <dareyblitz@vk.com>
2023-08-10 13:45:54 +0300

    add JUinit4 tests

---
+++ src/test/java/ru/netology/CashBackHackerTest.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+import junit.framework.TestCase;
+public class CashBackHackerTest {
+    @org.junit.Test
+    public void testRemain() {
+        CashBackHacker service = new CashBackHacker();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void testRemainWithCashBack() {
+        CashBackHacker service = new CashBackHacker();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+}
